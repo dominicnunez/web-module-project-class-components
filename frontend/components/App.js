632f452c6d256c6254381b2152c4e080c84e8b8d
Dominic A.N. <dominicnunez@gmail.com>
2024-10-01 13:02:58 -0500

    State, addTask, toggleCompleted, clearCompleted, html added

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,48 @@
 import React from 'react'
+import TodoList from './TodoList'
+import Form from './Form'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos: []
+    }
+  }
+  
+  addTask = (e, task) => {
+    e.preventDefault()
+    const newTask = {
+      name: task,
+      id: Date.now(),
+      completed: false
+    }
+
+    this.setState({...this.state, todos: [...this.state.todos, newTask]})
+  }
+
+  toggleCompleted = taskID => {
+    this.setState({...this.state, todos: this.state.todos.map(task => {
+      if (task.id == taskID) {
+        return {...task, purchased: !task.purchased}
+      }
+      return task;
+    })})
+}
+
+  clearCompleted = () => {
+    this.setState({...this.state, todos: this.state.todos.filter(task => !task.completed)})
+  }
+
   render() {
     return (
-      <div>
-        Todo App
-      </div>
+      <div className="App">
+        <div className="add-item">
+           <Form addItem={this.addTask} />
+         </div>
+        <TodoList toggleCompleted={this.toggleCompleted} todos={this.state.todos} />
+        <button className="clear-btn" onClick={this.clearCompleted}>Clear Completed</button>
+       </div>
     )
   }
 }
